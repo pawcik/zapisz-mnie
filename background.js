15daf8c9bd95e1254a6a21fb84a413a3931efa54
pawel-jedruch-miquido <pawel.jedruch@miquido.com>
2021-05-10 10:49:43 +0200

    Initial version of the code

--- background.js
+++ background.js
@@ -1,85 +1,80 @@
-function checkSlots() {
-  console.log('test');
-  //fetch("https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlots/find", {
-    //method: "POST",
-    //body: "{\"dayRange\":{\"from\":\"2021-05-07\",\"to\":\"2021-06-30\"},\"geoId\":\"1261011\",\"prescriptionId\":\"df063786-5209-4edc-a21e-260b77ff4df8\",\"voiId\":\"12\",\"vaccineTypes\":[\"cov19.pfizer\",\"cov19.moderna\",\"cov19.johnson_and_johnson\"]}",
-    //mode: "cors",
-    //credentials: 'include'
-  //});
-}
-
-function findSlots(headers) {
-  headers.origin = 'https://pacjent.erejestracja.ezdrowie.gov.pl';
-  fetch("https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlots/find", {
-    method: "POST",
-    headers: headers,
-    body: "{\"dayRange\":{\"from\":\"2021-05-07\",\"to\":\"2021-06-30\"},\"geoId\":\"1261011\",\"prescriptionId\":\"df063786-5209-4edc-a21e-260b77ff4df8\",\"voiId\":\"12\",\"vaccineTypes\":[\"cov19.pfizer\",\"cov19.moderna\",\"cov19.johnson_and_johnson\"]}"
-  });
-}
-
-var done = false;
-var xcsrftoken = undefined;
-
+var FIND_BODY = undefined;
 chrome.runtime.onInstalled.addListener(() => {
-  // create alarm after extension is installed / upgraded
-  //setInterval(checkSlots, 1000);
 
-  chrome.cookies.get({
-    url: "https://pacjent.erejestracja.ezdrowie.gov.pl",
-    name: "patient_sid"
-  }, (cookie) => {
-    console.log(cookie);
+  //cleaup session rules on install
+  chrome.declarativeNetRequest.getSessionRules((rules) => {
+    console.log('after install');
+    console.log(rules);
+    if (rules) {
+      const toRemoveIds = rules.map((r) => r.id);
+      chrome.declarativeNetRequest.updateSessionRules({
+        removeRuleIds: toRemoveIds
+      });
+    }
   });
 
   chrome.webRequest.onBeforeSendHeaders.addListener(
-    (details) => {
-      console.log(details.requestHeaders);
+    async (details) => {
       const headers = details.requestHeaders.reduce((map, obj) => {
         map[obj.name.toLowerCase()] = obj.value;
         return map;
       }, {});
-      if (!xcsrftoken) {
-        chrome.declarativeNetRequest.updateDynamicRules({
-          addRules: [
-            {
-              id: 1,
-              priority: 1,
-              action: {
-                type: 'modifyHeaders',
-                requestHeaders: [
-                  {
-                    header: 'x-csrf-token',
-                    operation: 'set',
-                    value: headers['x-csrf-token']
+      let xcsrftoken = headers['x-csrf-token'];
+      const regex = /.*api\/auth\/logout$/;
+      let isLogout = details.url.match(regex);
+      if (xcsrftoken && !isLogout) {
+        chrome.declarativeNetRequest.getSessionRules((count) => {
+          if (count == 0) {
+            chrome.declarativeNetRequest.updateSessionRules({
+              addRules: [
+                {
+                  id: 1,
+                  priority: 1,
+                  action: {
+                    type: 'modifyHeaders',
+                    requestHeaders: [
+                      {
+                        header: 'x-csrf-token',
+                        operation: 'set',
+                        value: xcsrftoken
+                      }
+                    ]
+                  },
+                  condition: {
+                    regexFilter: 'https://pacjent.erejestracja.ezdrowie.gov.pl/api/.*'
                   }
-                ]
-              },
-              condition: {
-                regexFilter: 'https://pacjent.erejestracja.ezdrowie.gov.pl/.*'
-              }
-            }
-          ]
-        }, (result) => {
-          console.log('created', result);
-        });
-        xcsrftoken = headers['x-csrf-token'];
-      } else {
-        details.requestHeaders.push({
-          name: 'x-csrf-token',
-          value: xcsrftoken
+                }
+              ]
+            }, (result) => {
+              console.log('created', result);
+            });
+          }
         });
       }
-      return {requestHeaders: details.requestHeaders}
     },
-    {urls: ["https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlots/find"]},
+    {urls: ["https://pacjent.erejestracja.ezdrowie.gov.pl/api/*"]},
     ["requestHeaders"]
   );
 
-  chrome.webRequest.onSendHeaders.addListener(
-    (details) => {
-      console.log(details.requestHeaders['x-csrf-token']);
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    async (details) => {
+      let rawBody = details.requestBody.raw[0].bytes;
+      let str = String.fromCharCode.apply(null, new Uint8Array(rawBody));
+      let findBody = JSON.parse(str);
+      FIND_BODY = findBody;
     },
     {urls: ["https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlots/find"]},
-    ["extraHeaders" ,"requestHeaders"]
+    ["requestBody"]
   );
+
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.sendMessage(tab.id, {
+        type: "search",
+        body: FIND_BODY
+      }, (response) => {
+        console.log(response);
+      });
+  });
+
 });
--- inject.js
+++ inject.js
@@ -1,48 +1,65 @@
-async function checkSlot() {
+async function checkSlot(body, callback) {
     let res = await fetch(
       "https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlots/find", {
       method: "POST",
-      body: `
-        {
-          "dayRange": {
-            "from": "2021-05-07",
-            "to": "2021-05-15"
-          },
-          "geoId": "1261011",
-          "prescriptionId": "df063786-5209-4edc-a21e-260b77ff4df8",
-          "voiId": "12",
-          "vaccineTypes": [
-            "cov19.pfizer",
-            "cov19.moderna",
-            "cov19.johnson_and_johnson"
-          ]
-        }`
+      body: JSON.stringify(body)
     });
+    let prescriptionId = body.prescriptionId;
     let slots = await res.json();
     console.log('Checking avaiable slots, got:');
     console.log(slots);
     if (slots && slots.list && slots.list.length > 0) {
       let first = slots.list[0];
+      let data = { prescriptionId: prescriptionId };
       res = await fetch(
         `https://pacjent.erejestracja.ezdrowie.gov.pl/api/calendarSlot/${first.id}/confirm`, {
         method: "POST",
-        body: `
-          {
-            "prescriptionId": "df063786-5209-4edc-a21e-260b77ff4df8"
-          }`
+        body: JSON.stringify(data)
       });
       let confirmRes = await res.json();
-      console.log(confirmRes);
-		  clearInterval(checkSlotIntervalId);
+      callback(res, confirmRes)
     }
 };
 
 var checkSlotIntervalId = undefined;
 
-var readyStateCheckInterval = setInterval(function() {
+function registerTab() {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    (tabs) => {
+      let tabId = tabs[0].id;
+      chrome.runtime.sendMessage(
+        {
+          type: 'register',
+          tabId: tab.id
+        },
+        (response) => {
+            if (response) {
+              this.setState({
+                traffic: Object.assign(this.state.traffic, response)
+              });
+            }
+          });
+    });
+}
+
+var readyStateCheckInterval = setInterval(() => {
   if (document.readyState === "complete") {
     clearInterval(readyStateCheckInterval);
-    checkSlotIntervalId = setInterval(checkSlot, 5000);
   }
 }, 10);
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "search") {
+    const search = () => {
+      checkSlot(request.body, (res, json) => {
+        console.log("Fouund!");
+        console.log(res);
+        console.log(json);
+        clearInterval(checkSlotIntervalId);
+      });
+
+    }
+    checkSlotIntervalId = setInterval(search, 5000);
+  }
+});
